d5595b66895b9606486328f9092b5c646b0fe836
TobiaRigon <rigontobia@gmail.com>
2024-02-21 12:08:48 +0100

    Bootstrap setup style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,22 @@
 @extends('layouts.app')
 @section('content')
 
-<h1 class="my-4">Progetti: {{ count($projects) }}</h1>
+<div class="container">
+    <h1 class="my-4">Progetti: {{ count($projects) }}</h1>
 
-<ul class = "list-unstyled">
-    @foreach ($projects as $project)
-    <li class="my-3 border">
-        <strong>nome progetto:</strong> {{ $project -> name }} <br>
-        <strong>descrizione:</strong> {{ $project -> description }} <br>
-        <strong>immagine:</strong> {{ $project -> image }} <br>
-        <strong>collaboratori:</strong> {{ $project -> collaborators }} <br>
-        <strong>leader del progetto:</strong> {{ $project -> projectLeader }} <br>
-    </li>
-    @endforeach
-</ul>
+    <div>
+        @foreach ($projects as $project)
+        <div class="card p-2 my-3 border">
+            <h2 class="text-uppercase text-primary font-weight-bold">{{ $project->name }}</h2>
+            <p>{{ $project->description }}</p>
+            <img src="{{ $project->image }}" class="p-5 text-center img-fluid" alt="Immagine del progetto"><br>
+            <h5 class="font-weight-bold">Collaboratori:</h5> <span class="font-italic">{{ $project->collaborators }}</span><br>
+            <h5 class="font-weight-bold">Leader del progetto:</h5> <span class="font-italic">{{ $project->projectLeader }}</span><br>
+        </div>
+        @endforeach
+    </div>
+
+</div>
+
 
 @endsection
